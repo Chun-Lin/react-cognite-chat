0ea7f2374eef9c2164fd9a1d2216d82c6c1d6e0b
Chun-Lin <wulin40063@gmail.com>
2020-11-04 20:44:49 +0800

    feat: 🎸 add userReducer to handle login and logout in redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react'
-import logo from './logo.svg'
-import { Counter } from 'containers/counter/Counter'
+import React, { useEffect } from 'react'
+
 import './App.css'
 import Messenger from 'containers/Messenger/Messenger'
+import Login from 'components/Login'
+import { auth } from 'firebaseSetting'
+import { login, logout, selectUser } from 'redux/user/userRedux'
+import { useDispatch, useSelector } from 'react-redux'
 
 function App() {
-  return (
-    <div className="App">
-      <Messenger />
-    </div>
-  )
+  const dispatch = useDispatch()
+  const user = useSelector(selectUser)
+
+  useEffect(() => {
+    auth.onAuthStateChanged(({ uid, photoURL, email, displayName }) => {
+      console.log('uid', uid)
+      console.log('photo', photoURL)
+      if (uid) {
+        dispatch(
+          login({
+            uid,
+            photoURL,
+            email,
+            displayName,
+          })
+        )
+      } else {
+        dispatch(logout())
+      }
+    })
+  }, [])
+
+  return <div className="App">{user ? <Messenger /> : <Login />}</div>
 }
 
 export default App
