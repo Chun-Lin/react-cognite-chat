c84668083ba47ab70cccc4722acc95d7d26af607
Chun-Lin <wulin40063@gmail.com>
2020-11-04 15:08:21 +0800

    refactor: 💡 move counter to container because it connects redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from './logo.svg'
-import { Counter } from 'redux/counter/Counter'
+import { Counter } from 'containers/counter/Counter'
 import './App.css'
 
 function App() {
