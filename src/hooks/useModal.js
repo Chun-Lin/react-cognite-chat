ccd3bcba77bcb213e402ee433243104d6a4efe83
Chun-Lin <wulin40063@gmail.com>
2020-11-04 17:50:41 +0800

    feat: 🎸 add useModal custom hooks to render comp on diff DOM
    add useModal custom hooks to render modal components on different DOM
    tree

---
+++ src/hooks/useModal.js
@@ -0,0 +1,77 @@
+import React, { memo, useState, useEffect, useCallback } from 'react'
+import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+
+const ModalPortalStyles = styled.div`
+  > *:first-child {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1000;
+    ${(props) => props.modalStyles};
+  }
+`
+
+let modalRootEl = document.getElementById('modal-root')
+if (!modalRootEl) {
+  modalRootEl = document.createElement('div')
+  modalRootEl.setAttribute('id', 'modal-root')
+}
+
+export const ModalPortal = memo(({ children }) => {
+  useEffect(() => {
+    document.body.appendChild(modalRootEl)
+
+    return () => document.body.removeChild(modalRootEl)
+  })
+
+  return ReactDOM.createPortal(
+    <ModalPortalStyles>{children}</ModalPortalStyles>,
+    modalRootEl
+  )
+})
+
+const ModalCover = styled.div`
+  position: absolute;
+  right: 0;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  z-index: 900;
+  background: ${(props) =>
+    props.isCoverShown
+      ? props.theme.modal.cover.backgroundColor
+      : 'transparent'};
+  ${(props) => props.coverStyles};
+`
+
+export const useModal = ({ isCoverRemovable = true, isCoverShown = true }) => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const showModal = useCallback(() => setIsVisible(true), [])
+  const closeModal = useCallback(() => setIsVisible(false), [])
+
+  const renderModal = (modal) => (
+    <>
+      <ModalPortal>
+        {isVisible ? (
+          <>
+            {modal}
+            <ModalCover
+              isCoverShown={isCoverShown}
+              onClick={isCoverRemovable ? closeModal : undefined}
+            />
+          </>
+        ) : null}
+      </ModalPortal>
+    </>
+  )
+
+  return {
+    isVisible,
+    showModal,
+    closeModal,
+    renderModal,
+  }
+}
