b062cbd2aaa9af8bbc6e65bd70d5cfbd5a95f7c8
Chun-Lin <wulin40063@gmail.com>
2020-11-04 17:47:21 +0800

    feat: 🎸 bring in firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/analytics'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAzDz5rbgAq6X88wy9H7r3ENRUpeTwm5T4',
+  authDomain: 'react-chat-app-79254.firebaseapp.com',
+  databaseURL: 'https://react-chat-app-79254.firebaseio.com',
+  projectId: 'react-chat-app-79254',
+  storageBucket: 'react-chat-app-79254.appspot.com',
+  messagingSenderId: '784510638433',
+  appId: '1:784510638433:web:9480b891297bd466b50d20',
+  measurementId: 'G-LTFNFX215H',
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const db = firebase.firestore()
+export const analytics = firebase.analytics()
+export const auth = firebase.auth()
+export const googleProvider = new firebase.auth.GoogleAuthProvider()
+
+export default db
