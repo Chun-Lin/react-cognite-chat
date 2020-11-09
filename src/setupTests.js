dd3d1aafc0203c76222018ce0c96f4f320f8c55c
Chun-Lin <wulin40063@gmail.com>
2020-11-09 16:05:09 +0800

    test: 💍 add mock data for firebase related apis

--- src/setupTests.js
+++ src/setupTests.js
@@ -12,10 +12,30 @@ jest.mock('firebase/app', () => {
     signInWithRedirect: jest.fn(),
     signInWithPopup: jest.fn(),
     onAuthStateChanged: jest.fn(),
+    signOut: jest.fn(),
   })
 
-  const firestore = jest.fn().mockReturnValue({})
+  // firestore mocks somehow doesn't work
+  const firestore = jest.fn().mockReturnValue({
+    collection: jest.fn().mockReturnValue({
+      doc: jest.fn().mockReturnValue({
+        add: jest.fn(),
+        onSnapshot: jest.fn(),
+        get: jest.fn(),
+        set: jest.fn(),
+      }),
+      where: jest.fn().mockReturnValue({
+        get: jest.fn(),
+        onSnapshot: jest.fn(),
+      }),
+    }),
+  })
+
   auth.GoogleAuthProvider = jest.fn()
 
   return { auth, initializeApp, firestore }
+})
+
+afterEach(() => {
+  jest.restoreAllMocks()
 })
